2e3992aaae7f45c02fa736c1512c6fa1e79da697
vProjects <github@vyrazu.com>
2014-06-13 13:52:36 +0530

    creating insert pages -anand

---
+++ v-panel/create-curriculum.php
@@ -0,0 +1,63 @@
+<?php
+	$title = 'Create Courses';
+	//include the template files
+	include 'v-templates/header.php';
+	include 'v-templates/sidebar.php';
+?>
+<div id="page-wrapper">
+    <div class="row">
+        <div class="col-lg-12">
+            <h1 class="page-header">Create Curriculum</h1>
+            <h4 class="cs_page_info">You can add data about curriculum manualy or you can just upload file directly.</h4>
+        </div>
+        <!-- /.col-lg-12 -->
+    </div>
+    <!-- /.row -->
+    <div class="row stu_adm_row">
+        <div class="col-lg-6">
+        	<form role="form">
+            	<h4 class="cs_page_form_caption">Fill Up curriculum Details</h4>
+            	<div class="form-group">
+                    <label class="cs_form_label">Name</label>
+                    <input type="text" class="form-control cs_form_textbox">
+                </div>
+                <div class="form-group">
+                    <label class="cs_form_label">Session</label>
+                    <input type="text" class="form-control cs_form_textbox">
+                </div>
+                <div class="form-group">
+                    <label class="cs_form_label">Time Duration</label>
+                    <input type="text" class="form-control cs_form_textbox">
+                </div>
+                <div class="form-group">
+                    <label class="cs_form_label">Course Details</label>
+                    <textarea rows="4" class="form-control ae_form_textarea"></textarea>
+                </div>
+                <button type="submit" class="btn btn-success btn-lg">Submit Data</button>
+                <button type="reset" class="btn btn-danger btn-lg">Reset Data</button>
+            </form>
+        </div>
+        <!-- /.col-lg-6 -->
+        <div class="col-lg-6">	
+        	<form role="form" enctype="multipart/form-data">
+            	<h4 class="cs_page_form_caption">Upload The File</h4>
+                <div class="form-group">
+                    <label class="cs_form_label">Information File</label>
+                    <input type="file">
+                </div>
+                <div class="form-group">
+                    <label class="cs_form_label">NOTE</label>
+                    <input type="text" class="form-control cs_form_textbox" placeholder="Upload Info">
+                </div>
+                <button type="submit" class="btn btn-success btn-lg">Submit</button>
+            </form>
+        </div>
+        <!-- /.col-lg-6 -->
+    </div>
+    <!-- /.row -->
+</div>
+<!-- /#page-wrapper -->
+<?php
+	//footer
+	include 'v-templates/footer.php';
+?>
